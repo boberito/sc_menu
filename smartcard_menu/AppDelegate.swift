77ec75bf2c4859cb63096db2f68d883e1a859628
Bob Gendler <boberito@mac.com>
2024-02-20 09:43:27 -0500

    Added being an actual menu
    * Added Quit
    * Now shows reader connected with submenu

--- smartcard_menu/AppDelegate.swift
+++ smartcard_menu/AppDelegate.swift
@@ -21,9 +21,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     var myTKWatcher: TKTokenWatcher? = nil
-    
+    var slotName: String?
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
+    let nothingInsertedMenu = NSMenuItem(title: "No Smartcard Inserted", action: nil, keyEquivalent: "")
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         
@@ -34,22 +34,59 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         update()
         statusItem.menu = NSMenu()
-//        let itemOne = NSMenuItem(title: "Stuff", action: #selector(stuff), keyEquivalent: "")
-//        statusItem.menu?.addItem(itemOne)
+        statusItem.menu?.insertItem(nothingInsertedMenu, at: 0)
+        let quitMenu = NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "")
+        statusItem.menu?.addItem(quitMenu)
+        if let fileURLString = Bundle.main.path(forResource: "smartcard_out", ofType: "png") {
+            let fileExists = FileManager.default.fileExists(atPath: fileURLString)
+            //                                removeReaderMenu()
+            if fileExists {
+                if let button = self.statusItem.button {
+                    button.image = NSImage(byReferencingFile: fileURLString)
+                }
+            } else {
+                self.statusItem.button?.title = "NOT Inserted"
+            }
+        }
+        
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
+    
+    func showReader(TkID: String) {
+        if let readerName = myTKWatcher?.tokenInfo(forTokenID: TkID)?.slotName {
+            let readerMenuItem = NSMenuItem(title: readerName, action: nil, keyEquivalent: "")
+            let readerMenuItemExists = statusItem.menu?.item(withTitle: readerName)
+            if readerMenuItemExists == nil {
+                statusItem.menu?.removeItem(at: 0)
+                let subMenu = NSMenu()
+                statusItem.menu?.insertItem(readerMenuItem, at: 0)
+                statusItem.menu?.setSubmenu(subMenu, for:  readerMenuItem)
+                let something = NSMenuItem(title: "CERT 1", action: nil, keyEquivalent: "")
+                subMenu.addItem(something)
+            } else {
+                return()
+            }
+            
+        }
+    }
+    
+    @objc func quit() {
+        exit(0)
+    }
     @objc func update() {
+        
         if let tokenCount = myTKWatcher?.tokenIDs, let tokenWatcher = myTKWatcher {
             for CTKTokenID in tokenCount {
+                
                 if tokenWatcher.tokenInfo(forTokenID: CTKTokenID)?.slotName != nil {
+                    slotName = tokenWatcher.tokenInfo(forTokenID: CTKTokenID)?.slotName
                     if let fileURLString = Bundle.main.path(forResource: "smartcard_in", ofType: "png") {
                         let fileExists = FileManager.default.fileExists(atPath: fileURLString)
                             if fileExists {
@@ -61,23 +98,37 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                                 statusItem.button?.title = "Inserted"
                             }
                     }
-                
-                    myTKWatcher?.addRemovalHandler(myHandler, forTokenID: CTKTokenID)
+                    
+                    showReader(TkID: CTKTokenID)
+                    
+                    
+                    myTKWatcher?.addRemovalHandler({ _ in
+                        
+                        RunLoop.main.perform {
+                            if let fileURLString = Bundle.main.path(forResource: "smartcard_out", ofType: "png") {
+                                let fileExists = FileManager.default.fileExists(atPath: fileURLString)
+                                //                                removeReaderMenu()
+                                if fileExists {
+                                    if let button = self.statusItem.button {
+                                        button.image = NSImage(byReferencingFile: fileURLString)
+                                    }
+                                } else {
+                                    self.statusItem.button?.title = "NOT Inserted"
+                                }
+                            }
+                            
+                            if let slotName = self.slotName {
+                                if let menuIndex = self.statusItem.menu?.indexOfItem(withTitle: slotName) {
+                                    self.statusItem.menu?.removeItem(at: menuIndex)
+                                    self.statusItem.menu?.insertItem(self.nothingInsertedMenu, at: 0)
+                                }
+                            }
+                        }
+                    }, forTokenID: CTKTokenID)
                     return()
+                    
                 }
                 
-            }
-
-            if let fileURLString = Bundle.main.path(forResource: "smartcard_out", ofType: "png") {
-                let fileExists = FileManager.default.fileExists(atPath: fileURLString)
-                    if fileExists {
-                        if let button = statusItem.button {
-                            
-                            button.image = NSImage(byReferencingFile: fileURLString)
-                        }
-                    } else {
-                        statusItem.button?.title = "NOT Inserted"
-                    }
             }
 
         }
