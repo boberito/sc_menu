be0baccd7787548d1130c95f1f3bf32ff082c732
Bob Gendler <boberito@mac.com>
2024-03-03 22:25:03 -0500

    Preferences menu
    - SMAppServices login item
    - Choice of colorful vs bw icon
    - App info

--- smartcard_menu/PreferencesViewController.swift
+++ smartcard_menu/PreferencesViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Bob Gendler on 3/2/24.
 //
-
+import ServiceManagement
 import Cocoa
 
 class PreferencesViewController: NSViewController {
@@ -21,20 +21,68 @@ class PreferencesViewController: NSViewController {
         iconLabel.isEditable = false
         iconLabel.drawsBackground = false
         
-        let startUpLabel = NSTextField(frame: NSRect(x: 200, y: 160, width: 150, height: 25))
-        startUpLabel.stringValue = "Start at Login"
-        startUpLabel.isBordered = false
-        startUpLabel.isBezeled = false
-        startUpLabel.isEditable = false
-        startUpLabel.drawsBackground = false
+        let iconOneImageOut = NSImageView(frame:NSRect(x: 25, y:130, width: 50, height: 40))
+        iconOneImageOut.image = NSImage(named: "smartcard_out")
+        let iconOneImageIn = NSImageView(frame:NSRect(x: 60, y:130, width: 50, height: 40))
+        iconOneImageIn.image = NSImage(named: "smartcard_in")
 
+        
+        let iconTwoImageOut = NSImageView(frame:NSRect(x: 25, y:75, width: 50, height: 40))
+        iconTwoImageOut.image = NSImage(named: "smartcard_out_bw")
+        let iconTwoImageIn = NSImageView(frame:NSRect(x: 60, y:75, width: 50, height: 40))
+        iconTwoImageIn.image = NSImage(named: "smartcard_in_bw")
+        
+        let iconOneRadioButton = NSButton(radioButtonWithTitle: "Color", target: Any?.self, action: #selector(changeIcon))
+        iconOneRadioButton.frame = NSRect(x: 25, y: 110, width: 150, height: 25)
+        iconOneRadioButton.title = "Option 1"
+        
+        let iconTwoRadioButton = NSButton(radioButtonWithTitle: "Color", target: Any?.self, action: #selector(changeIcon))
+        iconTwoRadioButton.frame = NSRect(x: 25, y: 50, width: 150, height: 25)
+        iconTwoRadioButton.title = "Option 2"
+        
         let startUpButton = NSButton(checkboxWithTitle: "Start at Login", target: Any?.self, action: #selector(loginItemChange))
         startUpButton.frame = NSRect(x: 200, y: 160, width: 150, height: 25)
+        switch SMAppService.mainApp.status {
+            case .enabled:
+                startUpButton.intValue = 1
+            
+            case .notFound:
+                startUpButton.intValue = 0
+            
+            case .notRegistered:
+                startUpButton.intValue = 0
+            
+            case .requiresApproval:
+                startUpButton.intValue = 0
+            
+            default:
+                startUpButton.intValue = 0
+        }
+        let infoVersionLabel = NSTextField(frame: NSRect(x: 150, y: 50, width: 200, height: 100))
+        if let versionText = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            let infoString = """
+    Version: \(versionText)
+    
+    https://github.com/boberito/sc_menu
+    """
+            let infoAttributedString = NSAttributedString(string: infoString)
+            infoVersionLabel.attributedStringValue = infoAttributedString
+        }
+        infoVersionLabel.isBordered = true
+        infoVersionLabel.isEditable = false
+        infoVersionLabel.lineBreakStrategy = .standard
+        infoVersionLabel.drawsBackground = false
+        
         
         view.addSubview(iconLabel)
-//        view.addSubview(startUpLabel)
         view.addSubview(startUpButton)
-//
+        view.addSubview(iconOneRadioButton)
+        view.addSubview(iconTwoRadioButton)
+        view.addSubview(iconOneImageOut)
+        view.addSubview(iconOneImageIn)
+        view.addSubview(iconTwoImageOut)
+        view.addSubview(iconTwoImageIn)
+        view.addSubview(infoVersionLabel)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,8 +95,25 @@ class PreferencesViewController: NSViewController {
         }
     }
     
-    @objc func loginItemChange() {
-        print("blah blah blah")
+    @objc func changeIcon(_ sender: NSButton) {
+        //use UserDefaults
+        
+    }
+    
+    @objc func loginItemChange(_ sender: NSButton) {
+        if sender.intValue == 1 {
+            do {
+                try SMAppService.mainApp.register()
+            } catch {
+                print("register error")
+            }
+        } else {
+            do {
+                try SMAppService.mainApp.unregister()
+            } catch {
+                print("unregister error")
+            }
+        }
     }
 
 
